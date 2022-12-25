40ca4d34908f0e2570a19a0dc09b88b7ab5cc799
Richard Maloley II <richard.maloley@gmail.com>
2022-12-25 13:39:22 -0500

    New simple script to extract the RockYou wordlist.

---
+++ packer/scripts/kali-gunzip-rockyou.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##########################
+# kali-gunzip-rockyou.sh #
+##########################
+
+echo "[->] kali-gunzip-rockyou.sh"
+
+echo "[*] Extracting the RockYou wordlist."
+gunzip /usr/share/wordlists/rockyou.txt.gz
+
+echo "[*] Done."
