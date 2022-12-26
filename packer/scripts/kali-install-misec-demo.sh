931f0011dbcecadf9343e1549ca39f2948207b49
Richard Maloley II <richard.maloley@gmail.com>
2022-12-26 15:58:55 -0500

    Added script to install our repo.

---
+++ packer/scripts/kali-install-misec-demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##############################
+# kali-install-misec-demo.sh #
+##############################
+
+echo "[->] kali-install-misec-demo.sh"
+
+echo "[*] Installing misec-demo repo."
+cd
+git clone https://github.com/maloleyr/misec-gr-automation-demo.git
+echo "[*] Done."
